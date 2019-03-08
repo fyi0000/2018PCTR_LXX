36ce96af0b95140e3a592ebf686037b40c08b520
fyi0000 <fyi0000@alu.ubu.es>
2019-03-08 11:44:05 +0100

    2a Clase 1aParte Resolve #4 Resolve #5

--- src/billar/Billiards.java
+++ src/billar/Billiards.java
@@ -21,7 +21,7 @@ public class Billiards extends JFrame {
 	private Board board;
 
 	// TODO update with number of group label. See practice statement.
-	private final int N_BALL = 2;
+	private final int N_BALL = 4+3;
 	private Ball[] balls;
 
 	public Billiards() {
@@ -56,6 +56,9 @@ public class Billiards extends JFrame {
 
 	private void initBalls() {
 		// TODO init balls
+		for (int i = 0; i < N_BALL; i++) {
+			balls[i]=new Ball();
+		}
 	}
 
 	private class StartListener implements ActionListener {
